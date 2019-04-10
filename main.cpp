262120bfb6da95893d901619058dd61af6a51785
Phil Hannent <phil@elmstonesystems.co.uk>
2019-04-10 10:46:46 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+    /*
+     * Rectangles
+     *
+     * The task is as follows:
+     *
+     * 1. create a class to represent a rectangle. A rectangle has x, y, width and height values.
+     *
+     * 2. create a function which takes as input a collection of rectangles, and returns the
+     *    sum of the areas of all the rectangles in the collection.
+     *
+     * 3. create a function which takes as input a collection of rectangles and returns the
+     *    a rectangle within which all the rectangles in the input collection fit tightly
+     *    (the bounding rectangle)
+     *
+     * You are welcome to use any standard libraries, functions or features which
+     * are included in the c++11 standard, as long as the requirements of the task are met.
+     * You are also allowed to use Qt classes if you are
+     * already familiar with the Qt API's (with some exceptions listed below). The choice of what
+     * to use to satisfy the "collection' requirement is also up to you. We do not mind the entire implementation
+     * being contained within this main.cpp file, or if you split it up into multiple files.
+     *
+     * To demonstrate the code is working, create a collection of example rectangles (2 or 3
+     * rectangles is enough) and print the result of the two functions on the console output
+     *
+     * Please feel free to ask questions if any of the above is unclear or if there
+     * is anything which you would like clarifying.
+     *
+     * The following classes must NOT be used:
+     *
+     * QRect, QRectF
+     */
+
+    std::cout << "test\n";
+
+    return 0;
+}
